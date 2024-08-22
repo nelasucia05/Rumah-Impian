31edbf04c20af5b16891472d3af49bb1a512368d
nelasucia05 <nelafah@gmail.com>
2024-08-22 11:26:43 +0700

    Add files via upload

--- Js/script.js
+++ Js/script.js
@@ -1,34 +1,37 @@
 const navbar = document.getElementsByTagName('nav')[0];
 window.addEventListener('scroll', function() {
     console.log(window.scrollY);
-    if (window.scrollY > 1){
+    if (window.scrollY > 1) {
         navbar.classList.replace('bg-transparent', 'nav-color');
-    } else if (this.window.scrollY <= 0) {
+    } else if (window.scrollY <= 0) {
         navbar.classList.replace('nav-color', 'bg-transparent');
     }
 });
+
+// Swiper instance for mySwiper
 var swiper = new Swiper(".mySwiper", {
-      slidesPerView: 3,
-      spaceBetween: 30,
-      pagination: {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
         el: ".swiper-pagination",
         clickable: true,
-      },
-      navigation: {
+    },
+    navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
-      },
-    });
+    },
+});
+
+// Swiper instance for testimonial-swiper
 var testimonialSwiper = new Swiper(".testimonial-swiper", {
     slidesPerView: 3,
-      spaceBetween: 30,
-      pagination: {
+    spaceBetween: 30,
+    pagination: {
         el: ".swiper-pagination",
         clickable: true,
-      },
-
+    },
     navigation: {
-        nextEl: ".swiper-buton-next",
-        prevEl: ".swiper-buton-prev",
+        nextEl: ".swiper-button-next",  // Corrected class name
+        prevEl: ".swiper-button-prev",  // Corrected class name
     },
 });
